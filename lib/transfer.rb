f6839c8b150dbff4c752513fb7081853b69e3fe5
Alli Tzadok <allisontzadok@gmail.com>
2018-01-23 21:06:50 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,13 @@
 class Transfer
-  # your code here
+
+#acts as a space for a transaction between two instances of the bank account class
+#will check the validity of the account number
+#will verify has enough funds to Transfer
+#will reject the transfer if they don't have enough funds
+
+#initializesas a pending status
+#status can change to completed or rejected
+#completed transfer can be reversed and status changed to reversed
+
+
 end
