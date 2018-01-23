2b14f9c08b81ab6da7e2a3711949192b3ec12ea0
Alli Tzadok <allisontzadok@gmail.com>
2018-01-23 21:28:55 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,14 +14,14 @@ end
 #will check the validity of the account number
 def valid?
   sender.valid? && receiver.valid?
-
 end
 
 #will verify has enough funds to Transfer
 #will reject the transfer if they don't have enough funds
+def execute_transaction
 
 
-
+end
 #status can change to completed or rejected
 #completed transfer can be reversed and status changed to reversed
 
